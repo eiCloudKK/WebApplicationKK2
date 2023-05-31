86856cad159852fe57a1d00681a3c7c152540ff5
eicloud <kalyan.krothapalli@jitterbit.com>
2023-05-31 17:22:27 -0400

    Merging WA4 into WA2

--- WebApplicationKK4/Pages/Error.cshtml.cs
+++ WebApplicationKK4/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace WebApplicationKK2.Pages
+namespace WebApplicationKK4.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
